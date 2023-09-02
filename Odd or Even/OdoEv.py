45b2c45bb765c6cca6d7b23986c85e540bcceb4c
jlfx09 <142736901+jlfx09@users.noreply.github.com>
2023-09-02 14:37:51 +0800

    Update OdoEv.py
    Updated comments as reference to Adding 2 Sums Program and Multiple Choice ASMD Folders in this repository.

--- Odd or Even/OdoEv.py
+++ Odd or Even/OdoEv.py
@@ -1,9 +1,12 @@
+#Refer to Adding 2 Sums Program for better understanding
+
 #assigns number as an integer input with the text "Enter a Number to be Processed".
 number = int(input("Enter a number to be processed "))
 
-#assigns 
+#assigns remainder as a variable that gets the remainder of number by 2.
 remainder = number % 2
 
+#If the remainder is 0, output that the number is an even number, if not then output that the result is an odd number with a remainder of the remainder remaining in the remainder variable.
 if (remainder == 0):
     print(number, "is an even number.")
 else:
